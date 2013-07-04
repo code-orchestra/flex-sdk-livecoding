15df1b85aa2493be216849553bb878da09ad6bb5
makc <makc.the.great@gmail.com>
2013-07-04 16:16:19 +0200

    arraylist clone fix

--- modules/asc/src/java/macromedia/asc/semantics/Slot.java
+++ modules/asc/src/java/macromedia/asc/semantics/Slot.java
@@ -665,7 +665,7 @@ public abstract class Slot implements Serializable, Cloneable // CodeOrchestra: 
         int n = src.size();
         ArrayList<MetaData> dst = new ArrayList<MetaData>(n);
         for (int i = 0; i < n; i++) {
-            dst.set(i, src.get(i).clone());
+            dst.add(src.get(i).clone());
         }
         return dst;
     }

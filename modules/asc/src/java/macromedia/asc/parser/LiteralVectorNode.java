29676362fdc96d640b7765fd4377b78b92e74d1c
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 15:35:38 +0200

    add clone for LiteralVectorNode

--- modules/asc/src/java/macromedia/asc/parser/LiteralVectorNode.java
+++ modules/asc/src/java/macromedia/asc/parser/LiteralVectorNode.java
@@ -67,4 +67,41 @@ public class LiteralVectorNode extends Node
 	{
 		return "LiteralVector";
 	}
+
+    public LiteralVectorNode clone() throws CloneNotSupportedException
+    {
+        LiteralVectorNode result = (LiteralVectorNode) super.clone();
+
+        if (elementlist != null) result.elementlist = elementlist.clone();
+        if (type != null) result.type = type.clone();
+        if (value != null) result.value = value.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        LiteralVectorNode that = (LiteralVectorNode) o;
+
+        if (void_result != that.void_result) return false;
+        if (elementlist != null ? !elementlist.equals(that.elementlist) : that.elementlist != null) return false;
+        if (type != null ? !type.equals(that.type) : that.type != null) return false;
+        if (value != null ? !value.equals(that.value) : that.value != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (elementlist != null ? elementlist.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (void_result ? 1 : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 }

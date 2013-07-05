c2c61ae461a76f061e3a5a546186b810ce7d438b
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:12:51 +0200

    add clone for BreakStatementNode

--- modules/asc/src/java/macromedia/asc/parser/BreakStatementNode.java
+++ modules/asc/src/java/macromedia/asc/parser/BreakStatementNode.java
@@ -57,4 +57,35 @@ public class BreakStatementNode extends Node
 	{
 		return "BreakStatement";
 	}
+
+    public BreakStatementNode clone() throws CloneNotSupportedException
+    {
+        BreakStatementNode result = (BreakStatementNode) super.clone();
+
+        if (id != null) result.id = id.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        BreakStatementNode that = (BreakStatementNode) o;
+
+        if (loop_index != that.loop_index) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + loop_index;
+        return result;
+    }
 }

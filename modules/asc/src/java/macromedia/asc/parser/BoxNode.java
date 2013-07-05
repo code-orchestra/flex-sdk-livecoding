18aaa41c609e0aa9fa48b4f4f208d4d8886b3102
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:25:37 +0200

    add clone for BoxNode

--- modules/asc/src/java/macromedia/asc/parser/BoxNode.java
+++ modules/asc/src/java/macromedia/asc/parser/BoxNode.java
@@ -62,4 +62,38 @@ public class BoxNode extends Node
 		void_result = true;
 		expr.voidResult();
 	}
+
+    public BoxNode clone() throws CloneNotSupportedException
+    {
+        BoxNode result = (BoxNode) super.clone();
+
+        if (actual != null) result.actual = actual.clone();
+        if (expr != null) result.expr = expr.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        BoxNode boxNode = (BoxNode) o;
+
+        if (void_result != boxNode.void_result) return false;
+        if (actual != null ? !actual.equals(boxNode.actual) : boxNode.actual != null) return false;
+        if (expr != null ? !expr.equals(boxNode.expr) : boxNode.expr != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (expr != null ? expr.hashCode() : 0);
+        result = 31 * result + (actual != null ? actual.hashCode() : 0);
+        result = 31 * result + (void_result ? 1 : 0);
+        return result;
+    }
 }

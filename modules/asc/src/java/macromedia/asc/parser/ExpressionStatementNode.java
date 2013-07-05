e8c26ab0e2ef8f60746a245e5a2b721c856eb3be
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 12:01:09 +0200

    add clone for ExpressionStatementNode and FunctionNameNode

--- modules/asc/src/java/macromedia/asc/parser/ExpressionStatementNode.java
+++ modules/asc/src/java/macromedia/asc/parser/ExpressionStatementNode.java
@@ -147,4 +147,47 @@ public class ExpressionStatementNode extends Node
 	{
 		return skip;
 	}
+
+    public ExpressionStatementNode clone() throws CloneNotSupportedException
+    {
+        ExpressionStatementNode result = (ExpressionStatementNode) super.clone();
+
+        if (expected_type != null) result.expected_type = expected_type.clone();
+        if (expr != null) result.expr = expr.clone();
+        if (gen_bits != null) result.gen_bits = BitSet.copy(gen_bits);
+        if (ref != null) result.ref = ref.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ExpressionStatementNode that = (ExpressionStatementNode) o;
+
+        if (is_var_stmt != that.is_var_stmt) return false;
+        if (skip != that.skip) return false;
+        if (expected_type != null ? !expected_type.equals(that.expected_type) : that.expected_type != null)
+            return false;
+        if (expr != null ? !expr.equals(that.expr) : that.expr != null) return false;
+        if (gen_bits != null ? !gen_bits.equals(that.gen_bits) : that.gen_bits != null) return false;
+        if (ref != null ? !ref.equals(that.ref) : that.ref != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (expr != null ? expr.hashCode() : 0);
+        result = 31 * result + (gen_bits != null ? gen_bits.hashCode() : 0);
+        result = 31 * result + (ref != null ? ref.hashCode() : 0);
+        result = 31 * result + (expected_type != null ? expected_type.hashCode() : 0);
+        result = 31 * result + (is_var_stmt ? 1 : 0);
+        result = 31 * result + (skip ? 1 : 0);
+        return result;
+    }
 }

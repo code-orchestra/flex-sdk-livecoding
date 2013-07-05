c361cbf3bb2959da3d9c0bc2dc88a6b90fa78439
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:31:36 +0200

    add clone for ThrowStatementNode

--- modules/asc/src/java/macromedia/asc/parser/ThrowStatementNode.java
+++ modules/asc/src/java/macromedia/asc/parser/ThrowStatementNode.java
@@ -67,4 +67,35 @@ public class ThrowStatementNode extends Node
 	{
 		return "ThrowStatement";
 	}
+
+    public ThrowStatementNode clone() throws CloneNotSupportedException
+    {
+        ThrowStatementNode result = (ThrowStatementNode) super.clone();
+
+        if (expr != null) result.expr = expr.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ThrowStatementNode that = (ThrowStatementNode) o;
+
+        if (finallyInserted != that.finallyInserted) return false;
+        if (expr != null ? !expr.equals(that.expr) : that.expr != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (expr != null ? expr.hashCode() : 0);
+        result = 31 * result + (finallyInserted ? 1 : 0);
+        return result;
+    }
 }

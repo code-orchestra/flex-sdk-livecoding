b4880e8c85ae4d2792ce5b4a0bb1e0e954d63035
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:03:00 +0200

    add clone for LiteralArrayNode

--- modules/asc/src/java/macromedia/asc/parser/LiteralArrayNode.java
+++ modules/asc/src/java/macromedia/asc/parser/LiteralArrayNode.java
@@ -64,4 +64,38 @@ public class LiteralArrayNode extends Node
 	{
 		return "LiteralArray";
 	}
+
+    public LiteralArrayNode clone() throws CloneNotSupportedException
+    {
+        LiteralArrayNode result = (LiteralArrayNode) super.clone();
+
+        if (elementlist != null) result.elementlist = elementlist.clone();
+        if (value != null) result.value = value.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        LiteralArrayNode that = (LiteralArrayNode) o;
+
+        if (void_result != that.void_result) return false;
+        if (elementlist != null ? !elementlist.equals(that.elementlist) : that.elementlist != null) return false;
+        if (value != null ? !value.equals(that.value) : that.value != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (elementlist != null ? elementlist.hashCode() : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (void_result ? 1 : 0);
+        return result;
+    }
 }

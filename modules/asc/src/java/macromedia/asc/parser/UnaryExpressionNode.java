9fc6ce008d20e69d691a2497d5fdc11574ff3a10
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:52:32 +0200

    add clone for UnaryExpressionNode

--- modules/asc/src/java/macromedia/asc/parser/UnaryExpressionNode.java
+++ modules/asc/src/java/macromedia/asc/parser/UnaryExpressionNode.java
@@ -73,4 +73,46 @@ public class UnaryExpressionNode extends Node
 	{
 		return "UnaryExpression";
 	}
+
+    public UnaryExpressionNode clone() throws CloneNotSupportedException
+    {
+        UnaryExpressionNode result = (UnaryExpressionNode) super.clone();
+
+        if (expr != null) result.expr = expr.clone();
+        if (numberUsage != null) result.numberUsage = numberUsage.clone();
+        if (ref != null) result.ref = ref.clone();
+        if (slot != null) result.slot = slot.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        UnaryExpressionNode that = (UnaryExpressionNode) o;
+
+        if (op != that.op) return false;
+        if (void_result != that.void_result) return false;
+        if (expr != null ? !expr.equals(that.expr) : that.expr != null) return false;
+        if (numberUsage != null ? !numberUsage.equals(that.numberUsage) : that.numberUsage != null) return false;
+        if (ref != null ? !ref.equals(that.ref) : that.ref != null) return false;
+        if (slot != null ? !slot.equals(that.slot) : that.slot != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (expr != null ? expr.hashCode() : 0);
+        result = 31 * result + op;
+        result = 31 * result + (ref != null ? ref.hashCode() : 0);
+        result = 31 * result + (slot != null ? slot.hashCode() : 0);
+        result = 31 * result + (numberUsage != null ? numberUsage.hashCode() : 0);
+        result = 31 * result + (void_result ? 1 : 0);
+        return result;
+    }
 }

0659847dd1fc2e95ba3ae1bcad048170e620c008
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:43:08 +0200

    add clone for LiteralObjectNode

--- modules/asc/src/java/macromedia/asc/parser/LiteralObjectNode.java
+++ modules/asc/src/java/macromedia/asc/parser/LiteralObjectNode.java
@@ -63,4 +63,35 @@ public class LiteralObjectNode extends Node
 	{
 		return "LiteralObject";
 	}
+
+    public LiteralObjectNode clone() throws CloneNotSupportedException
+    {
+        LiteralObjectNode result = (LiteralObjectNode) super.clone();
+
+        if (fieldlist != null) result.fieldlist = fieldlist.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        LiteralObjectNode that = (LiteralObjectNode) o;
+
+        if (void_result != that.void_result) return false;
+        if (fieldlist != null ? !fieldlist.equals(that.fieldlist) : that.fieldlist != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (fieldlist != null ? fieldlist.hashCode() : 0);
+        result = 31 * result + (void_result ? 1 : 0);
+        return result;
+    }
 }

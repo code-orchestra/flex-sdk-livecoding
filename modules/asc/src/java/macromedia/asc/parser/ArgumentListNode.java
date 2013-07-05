dac941abc6b464cb3f3a0c2b462662bd1a451b82
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 17:51:11 +0200

    fix clone and add equals for ArgumentListNode

--- modules/asc/src/java/macromedia/asc/parser/ArgumentListNode.java
+++ modules/asc/src/java/macromedia/asc/parser/ArgumentListNode.java
@@ -17,6 +17,7 @@
 
 package macromedia.asc.parser;
 
+import macromedia.asc.parser.util.CloneUtil;
 import macromedia.asc.util.*;
 import macromedia.asc.semantics.*;
 
@@ -108,24 +109,37 @@ public class ArgumentListNode extends Node
     {
         ArgumentListNode result = (ArgumentListNode) super.clone();
 
-        ObjectList<Node> items_cloned = new ObjectList<Node>(items.size());
-        for (Node item: items) items_cloned.add(item.clone());
-        result.items = items_cloned;
-
-        if (expected_types != null)
-        {
-            ObjectList<TypeInfo> expected_types_cloned = new ObjectList<TypeInfo>(expected_types.size());
-            for (TypeInfo info: expected_types) expected_types_cloned.add(info.clone());
-            result.expected_types = expected_types_cloned;
-        }
-
-        if (decl_styles != null)
-        {
-            result.decl_styles = new ByteList(decl_styles);
-        }
+        if (items != null) result.items = CloneUtil.cloneListNode(items);
+        if (expected_types != null) result.expected_types = CloneUtil.cloneListTypeInfo(expected_types);
+        if (decl_styles != null) result.decl_styles = new ByteList(decl_styles);
 
         return result;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
 
+        ArgumentListNode that = (ArgumentListNode) o;
+
+        if (is_bracket_selector != that.is_bracket_selector) return false;
+        if (decl_styles != null ? !decl_styles.equals(that.decl_styles) : that.decl_styles != null) return false;
+        if (expected_types != null ? !expected_types.equals(that.expected_types) : that.expected_types != null)
+            return false;
+        if (items != null ? !items.equals(that.items) : that.items != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (items != null ? items.hashCode() : 0);
+        result = 31 * result + (expected_types != null ? expected_types.hashCode() : 0);
+        result = 31 * result + (decl_styles != null ? decl_styles.hashCode() : 0);
+        result = 31 * result + (is_bracket_selector ? 1 : 0);
+        return result;
+    }
 }

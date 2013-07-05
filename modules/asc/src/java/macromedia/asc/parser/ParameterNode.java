91b8abf347830577a9266f9ee95b3907878ba99b
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 11:41:14 +0200

    add clone and equals for ParameterNode and subclasses

--- modules/asc/src/java/macromedia/asc/parser/ParameterNode.java
+++ modules/asc/src/java/macromedia/asc/parser/ParameterNode.java
@@ -69,4 +69,52 @@ public class ParameterNode extends Node
 	{
 		return "Parameter";
 	}
+
+    public ParameterNode clone() throws CloneNotSupportedException
+    {
+        ParameterNode result = (ParameterNode) super.clone();
+
+        if (attrs != null) result.attrs = attrs.clone();
+        if (identifier != null) result.identifier = identifier.clone();
+        if (init != null) result.init = init.clone();
+        if (ref != null) result.ref = ref.clone();
+        if (type != null) result.type = type.clone();
+        if (typeref != null) result.typeref = typeref.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ParameterNode that = (ParameterNode) o;
+
+        if (kind != that.kind) return false;
+        if (no_anno != that.no_anno) return false;
+        if (attrs != null ? !attrs.equals(that.attrs) : that.attrs != null) return false;
+        if (identifier != null ? !identifier.equals(that.identifier) : that.identifier != null) return false;
+        if (init != null ? !init.equals(that.init) : that.init != null) return false;
+        if (ref != null ? !ref.equals(that.ref) : that.ref != null) return false;
+        if (type != null ? !type.equals(that.type) : that.type != null) return false;
+        if (typeref != null ? !typeref.equals(that.typeref) : that.typeref != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + kind;
+        result = 31 * result + (identifier != null ? identifier.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (init != null ? init.hashCode() : 0);
+        result = 31 * result + (ref != null ? ref.hashCode() : 0);
+        result = 31 * result + (typeref != null ? typeref.hashCode() : 0);
+        result = 31 * result + (attrs != null ? attrs.hashCode() : 0);
+        result = 31 * result + (no_anno ? 1 : 0);
+        return result;
+    }
 }

6fb76c461c66077374075635ee64d086f8a89131
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 15:59:37 +0200

    add clone for TryStatementNode

--- modules/asc/src/java/macromedia/asc/parser/TryStatementNode.java
+++ modules/asc/src/java/macromedia/asc/parser/TryStatementNode.java
@@ -79,4 +79,45 @@ public class TryStatementNode extends Node
 	{
 		return skip;
 	}
+
+    public TryStatementNode clone() throws CloneNotSupportedException
+    {
+        TryStatementNode result = (TryStatementNode) super.clone();
+
+        if (catchlist != null) result.catchlist = catchlist.clone();
+        if (finallyblock != null) result.finallyblock = finallyblock.clone();
+        if (tryblock != null) result.tryblock = tryblock.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        TryStatementNode that = (TryStatementNode) o;
+
+        if (finallyInserted != that.finallyInserted) return false;
+        if (loop_index != that.loop_index) return false;
+        if (skip != that.skip) return false;
+        if (catchlist != null ? !catchlist.equals(that.catchlist) : that.catchlist != null) return false;
+        if (finallyblock != null ? !finallyblock.equals(that.finallyblock) : that.finallyblock != null) return false;
+        if (tryblock != null ? !tryblock.equals(that.tryblock) : that.tryblock != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (tryblock != null ? tryblock.hashCode() : 0);
+        result = 31 * result + (catchlist != null ? catchlist.hashCode() : 0);
+        result = 31 * result + (finallyblock != null ? finallyblock.hashCode() : 0);
+        result = 31 * result + (finallyInserted ? 1 : 0);
+        result = 31 * result + loop_index;
+        result = 31 * result + (skip ? 1 : 0);
+        return result;
+    }
 }

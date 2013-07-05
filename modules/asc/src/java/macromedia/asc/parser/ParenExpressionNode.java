7dc1a7b3c06c28d82f16a630bfddbd0a52ff08c6
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 15:26:30 +0200

    add clone for ParenExpressionNode

--- modules/asc/src/java/macromedia/asc/parser/ParenExpressionNode.java
+++ modules/asc/src/java/macromedia/asc/parser/ParenExpressionNode.java
@@ -56,4 +56,33 @@ public class ParenExpressionNode extends Node
 	{
 		return "ParenExpression";
 	}
+
+    public ParenExpressionNode clone() throws CloneNotSupportedException
+    {
+        ParenExpressionNode result = (ParenExpressionNode) super.clone();
+
+        if (expr != null) result.expr = expr.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ParenExpressionNode that = (ParenExpressionNode) o;
+
+        if (expr != null ? !expr.equals(that.expr) : that.expr != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (expr != null ? expr.hashCode() : 0);
+        return result;
+    }
 }

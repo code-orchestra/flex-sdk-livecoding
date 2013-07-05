5ddfb45f63e9bc3e38fd387bc4989e5d70470c72
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 15:20:53 +0200

    add clone for LiteralXMLNode

--- modules/asc/src/java/macromedia/asc/parser/LiteralXMLNode.java
+++ modules/asc/src/java/macromedia/asc/parser/LiteralXMLNode.java
@@ -65,4 +65,37 @@ public class LiteralXMLNode extends Node
 	{
 		return "LiteralXML";
 	}
+
+    public LiteralXMLNode clone() throws CloneNotSupportedException
+    {
+        LiteralXMLNode result = (LiteralXMLNode) super.clone();
+
+        if (list != null) result.list = list.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        LiteralXMLNode that = (LiteralXMLNode) o;
+
+        if (is_xmllist != that.is_xmllist) return false;
+        if (void_result != that.void_result) return false;
+        if (list != null ? !list.equals(that.list) : that.list != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (list != null ? list.hashCode() : 0);
+        result = 31 * result + (is_xmllist ? 1 : 0);
+        result = 31 * result + (void_result ? 1 : 0);
+        return result;
+    }
 }

e386726f084ba5c1c5e3728a3dc582251a4736c5
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 17:43:22 +0200

    add clone for InheritanceNode

--- modules/asc/src/java/macromedia/asc/parser/InheritanceNode.java
+++ modules/asc/src/java/macromedia/asc/parser/InheritanceNode.java
@@ -52,4 +52,36 @@ public class InheritanceNode extends Node
 	{
 		return "inheritance";
 	}
+
+    public InheritanceNode clone() throws CloneNotSupportedException
+    {
+        InheritanceNode result = (InheritanceNode) super.clone();
+
+        if (baseclass != null) result.baseclass = baseclass.clone();
+        if (interfaces != null) result.interfaces = interfaces.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        InheritanceNode that = (InheritanceNode) o;
+
+        if (baseclass != null ? !baseclass.equals(that.baseclass) : that.baseclass != null) return false;
+        if (interfaces != null ? !interfaces.equals(that.interfaces) : that.interfaces != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (baseclass != null ? baseclass.hashCode() : 0);
+        result = 31 * result + (interfaces != null ? interfaces.hashCode() : 0);
+        return result;
+    }
 }

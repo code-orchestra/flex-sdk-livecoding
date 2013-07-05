c357731623ddf4f29772f81a92fe1ee44f287d61
Dima Kruk <dima.kruk@gmail.com>
2013-07-05 16:30:42 +0200

    add clone for VariableBindingNode and TypedIdentifierNode

--- modules/asc/src/java/macromedia/asc/parser/VariableBindingNode.java
+++ modules/asc/src/java/macromedia/asc/parser/VariableBindingNode.java
@@ -78,4 +78,51 @@ public class VariableBindingNode extends Node
 	{
 		return "VariableBinding";
 	}
+
+    public VariableBindingNode clone() throws CloneNotSupportedException
+    {
+        VariableBindingNode result = (VariableBindingNode) super.clone();
+
+        if (attrs != null) result.attrs = attrs.clone();
+        // debug_name is String
+        //if (debug_name != null);
+        if (initializer != null) result.initializer = initializer.clone();
+        if (ref != null) result.ref = ref.clone();
+        if (typeref != null) result.typeref = typeref.clone();
+        if (variable != null) result.variable = variable.clone();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        VariableBindingNode that = (VariableBindingNode) o;
+
+        if (kind != that.kind) return false;
+        if (attrs != null ? !attrs.equals(that.attrs) : that.attrs != null) return false;
+        if (debug_name != null ? !debug_name.equals(that.debug_name) : that.debug_name != null) return false;
+        if (initializer != null ? !initializer.equals(that.initializer) : that.initializer != null) return false;
+        if (ref != null ? !ref.equals(that.ref) : that.ref != null) return false;
+        if (typeref != null ? !typeref.equals(that.typeref) : that.typeref != null) return false;
+        if (variable != null ? !variable.equals(that.variable) : that.variable != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (variable != null ? variable.hashCode() : 0);
+        result = 31 * result + (initializer != null ? initializer.hashCode() : 0);
+        result = 31 * result + (ref != null ? ref.hashCode() : 0);
+        result = 31 * result + (typeref != null ? typeref.hashCode() : 0);
+        result = 31 * result + (attrs != null ? attrs.hashCode() : 0);
+        result = 31 * result + (debug_name != null ? debug_name.hashCode() : 0);
+        result = 31 * result + kind;
+        return result;
+    }
 }
